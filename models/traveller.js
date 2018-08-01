fb1785c2210aa938f47077dca8a2ee530ac2b240
Gregor Cox <gregorcox@gmail.com>
2018-08-01 15:26:42 +0100

    can get journeys by transport

--- models/traveller.js
+++ models/traveller.js
@@ -15,7 +15,7 @@ Traveller.prototype.getModesOfTransport = function () {
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter(journey => journey.transport === transport);
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
